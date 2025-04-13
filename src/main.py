f115e7de93f1c8a743664bcf39cdf2db35d3ffc3
Евгений <zhenyayasakov@yandex.ru>
2025-04-13 16:31:14 +0500

    tests

--- src/main.py
+++ src/main.py
@@ -53,27 +53,6 @@ logging.config.dictConfig(logging_config)
 logger = logging.getLogger(__name__)
 
 
-
-"""def extract_data(spark, config):
-    # Загрузка данных с помощью Pandas для проверки на дубликаты
-    products_df = pd.read_csv(config['data']['products_path'])
-    categories_df = pd.read_csv(config['data']['categories_path'])
-    product_category_df = pd.read_csv(config['data']['product_category_path'])
-
-    # Удаляем дубликаты по 'product_id' и 'category_id' соответственно
-    products_df = products_df.drop_duplicates(subset='product_id')
-    categories_df = categories_df.drop_duplicates(subset='category_id')
-
-    # Преобразование обратно в Spark DataFrame
-    spark_products_df = spark.createDataFrame(products_df)
-    spark_categories_df = spark.createDataFrame(categories_df)
-    spark_product_category_df = spark.createDataFrame(product_category_df)
-
-    return spark_products_df, spark_categories_df, spark_product_category_df"""
-
-
-
-
 def edit_csv_file(file_path):
     """Редактирование CSV файла"""
     df = pd.read_csv(file_path)
